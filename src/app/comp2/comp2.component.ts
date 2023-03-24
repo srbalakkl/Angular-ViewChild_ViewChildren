019cb6d85bfa8aac541c244bb8389d4a80eddb54
sr.bala <sr.bala.kkl@gmail.com>
2023-03-24 09:11:46 +0530

    New:comp2

--- src/app/comp2/comp2.component.ts
+++ src/app/comp2/comp2.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {AppComponent} from "../app.component";
 
 @Component({
   selector: 'app-comp2',
@@ -7,9 +8,11 @@ import {Component, OnInit} from '@angular/core';
 })
 export class Comp2Component implements OnInit {
 
+  @ViewChild(AppComponent) ac!:AppComponent;
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
   check(){
